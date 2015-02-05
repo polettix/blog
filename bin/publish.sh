b5b8119d792222847e9d9f532f5d071dc4bace8c
Flavio Poletti <flavio@polettix.it>
2015-02-05 02:04:47 +0100

    simplified publishing script

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,32 +8,20 @@ die() {
    exit 1
 }
 
-outer_main() {
-   git checkout master
+main() {
 
    cd "$MYDIR" || die "unable to go in $MYDIR"
    cd .. || die "unable to go in parent directory of $MYDIR"
    echo "in $PWD now"
 
+   git checkout master || die 'unable to switch to master'
    bundle exec jekyll build || die "unable to update contents"
-
-   cp "$FULLME" _site || die "cannot handover to myself"
-   _site/"$BAREME" inner || die 'errors in handover'
+   git checkout gh-pages || die 'unable to switch to gh-pages'
+   tar cf - -C _site . | tar xvf - \
+   && git add . \
+   && git commit -m "$(date '+update at %Y%m%d-%H%M%S')" \
+   && git push origin gh-pages
+   git checkout master || die 'unable to switch to master'
 }
 
-inner_main() {
-   git checkout gh-pages
-   tar cf - -C _site . | tar xvf -
-   rm "$BAREME"
-   git add .
-   now=$(date '+%Y%m%d-%H%M%S')
-   git commit -m "update at $now"
-   git push origin gh-pages
-   git checkout master
-}
-
-if [ "$1" == "inner" ]; then
-   inner_main
-else
-   outer_main
-fi
+main
