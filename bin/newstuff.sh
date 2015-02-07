a95fe53726ce0fb0b616040cabbc61c3b4a888f9
Flavio Poletti <flavio@polettix.it>
2015-02-07 11:55:01 +0100

    fixed bug in newstuff.sh

--- bin/newstuff.sh
+++ bin/newstuff.sh
@@ -12,7 +12,7 @@ subpath="_drafts"
 [ "$(basename "$0")" == "newpost.sh" ] && subpath="_posts"
 fullpath=$(readlink -f "$MYDIR/../$subpath/$fullname")
 
-cat >"$fullpath" <<"END"
+cat >"$fullpath" <<END
 ---
 # vim: ts=4 sw=4 expandtab syn=off
 layout: post
