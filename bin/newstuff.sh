a5b1b4676aad82b210866fce37bbac16df5a8599
Flavio Poletti <flavio@polettix.it>
2015-09-01 23:31:40 +0200

    comments are activated by default on new posts

--- bin/newstuff.sh
+++ bin/newstuff.sh
@@ -27,6 +27,7 @@ image:
    feature: yellow-river.jpg
    credit: Yellow Water al Kakadu National Park, Australia
    creditlink: https://en.wikipedia.org/wiki/Kakadu_National_Park
+comments: true
 ---
 
 END
